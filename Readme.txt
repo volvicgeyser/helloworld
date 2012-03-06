74d85d756dd3f3a85923a28c65096e9a3bf4b2cc
Volvic <Fenrir@.(none)>
2012-03-06 23:04:36 +0900

    first commit

---
+++ Readme.txt
@@ -0,0 +1,18 @@
+ネタで書いたハローワールド集です
+These helloworld code are crazy.
+
+コンパイラを選びます。GCCとideoneで動作確認済み。
+
+
+言語：色々な予定
+
+
+著作権とライセンス
+Copyright & License
+
+Copyright (c) 2012, Volvic Geyser
+ All rights reserved.
+
+Twitter: http://twitter.com/romost
+
+License: The BSD 2-Clause License
---
+++ helloworld_chaos.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+#define _1 0
+#define _0 1
+#define _3 110
+#define __3 86
+#define ___3 45
+
+
+//volvic geyser
+int main()
+{
+		//typedef char ____; 
+		int i = sizeof(char) ^ sizeof(char);
+		char h = ((h == h) << (h == h) << (h == h) << (h == h) << (h == h)
+				<< (h == h) << (h == h)) + ((h == h) << (h == h) << (h == h) << (h == h));
+		char e;
+		char format[sizeof(char) / sizeof(char)
+				<< sizeof(char) / sizeof(char) + sizeof(char) / sizeof(char)];
+		format[(sizeof(char) != sizeof(char))] = 33 + 3 + sizeof(char) / sizeof(char);
+		format[(sizeof(char) == sizeof(char))] = 33 * 3;
+		format[(sizeof(format) - (sizeof(char) / sizeof(char) + (sizeof(char) == sizeof(char))))] = (sizeof(char) != sizeof(char));
+
+		printf("") == printf("") ? printf(format, h): printf("はろわのはろわ");
+		printf(format, e = h ^ 45);
+		printf(format, e ^ ((3 + 3 + 3 + 3 + 3 + 3) >> (printf(format, e ^ ((3 + 3 + 3 + 3 + 3 + 3) >> (h == h)))))); 
+		printf(format, 110 + printf(format, 86 + printf(format, (1 + 10 + 100))));
+		_1 == 0 ? 0 : printf("Bonjour");
+        for(i; i < '@' + 5 * 10; i++){}
+		printf(format, i);
+		return 0 ,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,printf("%c%c", 108, e - (
+										(
+										 (sizeof(char) == sizeof(char)) == (e == e)
+										)));
+}
